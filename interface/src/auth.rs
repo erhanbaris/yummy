f71546cd8ac112d5e02c12c683589a8827dbfd9a
Ruslan Asenov <Ruslan.Asenov@motorolasolutions.com>
2023-01-24 17:26:25 +0100

    Plugin changes

--- interface/src/auth.rs
+++ interface/src/auth.rs
@@ -2,6 +2,8 @@ use std::sync::Arc;
 
 use general::{auth::UserAuth, password::Password, client::ClientTrait};
 
+use crate::UserProxy;
+
 pub struct YummyEmailAuthModel {
     pub ref_id: usize,
     pub auth: Arc<Option<UserAuth>>,
@@ -12,15 +14,15 @@ pub struct YummyEmailAuthModel {
 }
 
 pub trait YummyAuthInterface {
-    fn pre_email_auth(&self, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> { Ok(model) }
-    fn post_email_auth(&self, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> { Ok(model) }
+    fn pre_email_auth<'a>(&self, user_manager: &'a dyn UserProxy, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> { Ok(model) }
+    fn post_email_auth<'a>(&self, user_manager: &'a dyn UserProxy, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> { Ok(model) }
 }
 
 #[derive(Default)]
 pub struct DummyYummyAuthPlugin;
 
 impl YummyAuthInterface for DummyYummyAuthPlugin {
-    fn pre_email_auth(&self, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> {
+    fn pre_email_auth<'a>(&self, user_manager: &'a dyn UserProxy, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> {
         let YummyEmailAuthModel { ref_id, auth, email, password, if_not_exist_create, socket } = model;
         println!("pre email auth");
         Ok(YummyEmailAuthModel {
@@ -33,8 +35,29 @@ impl YummyAuthInterface for DummyYummyAuthPlugin {
         })
     }
 
-    fn post_email_auth(&self, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> {
+    fn post_email_auth<'a>(&self, user_manager: &'a dyn UserProxy, model: YummyEmailAuthModel) -> anyhow::Result<YummyEmailAuthModel> {
         println!("post email auth");
         Ok(model)
+    }
+}
+
+#[derive(Default)]
+pub struct DummyUserProxy;
+
+impl UserProxy for DummyUserProxy {
+    fn get_user(&self, user: general::model::UserId) -> Option<database::model::UserInformationModel> {
+        todo!()
+    }
+
+    fn get_user_meta(&self, user: general::model::UserId, key: String) -> Option<general::meta::MetaType<general::meta::UserMetaAccess>> {
+        todo!()
+    }
+
+    fn set_user_meta(&self, user: general::model::UserId, key: String, meta: general::meta::MetaType<general::meta::UserMetaAccess>) -> Option<general::meta::MetaType<general::meta::UserMetaAccess>> {
+        todo!()
+    }
+
+    fn remove_user_meta(&self, user: general::model::UserId, key: String) {
+        todo!()
     }
 }
--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,11 +1,32 @@
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::{sync::atomic::{AtomicBool, Ordering}, collections::HashMap, ops::Deref};
 
 use auth::{YummyAuthInterface, YummyEmailAuthModel};
+use general::{model::{UserId, UserType}, meta::{MetaType, UserMetaAccess, MetaAction}};
+use database::model::UserInformationModel;
 
 pub mod auth;
 
-pub enum YummyAuthError {
+pub struct UpdateUser {
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub password: Option<String>,
+    pub device_id: Option<String>,
+    pub custom_id: Option<String>,
+    pub user_type: Option<UserType>,
+    pub meta: Option<HashMap<String, MetaType<UserMetaAccess>>>,
+    pub meta_action: Option<MetaAction>
+}
 
+pub trait UserProxy {
+    fn get_user(&self, user: UserId) -> Option<UserInformationModel>;
+    fn get_user_meta(&self, user: UserId, key: String) -> Option<MetaType<UserMetaAccess>>;
+    fn set_user_meta(&self, user: UserId, key: String, meta: MetaType<UserMetaAccess>) -> Option<MetaType<UserMetaAccess>>;
+    fn remove_user_meta(&self, user: UserId, key: String);
+}
+
+pub enum YummyAuthError {
+    AuthFailed(String),
+    Other(String)
 }
 
 pub struct PluginInfo {
@@ -14,12 +35,19 @@ pub struct PluginInfo {
     pub active: AtomicBool
 }
 
-#[derive(Default)]
 pub struct PluginExecuter {
-    pub auth_interfaces: Vec<PluginInfo>
+    user_manager: Box<dyn UserProxy>,
+    auth_interfaces: Vec<PluginInfo>
 }
 
 impl PluginExecuter {
+    pub fn new(user_manager: Box<dyn UserProxy>) -> Self {
+        Self {
+            user_manager,
+            auth_interfaces: Vec::new()
+        }
+    }
+
     pub fn add_auth_plugin(&mut self, name: String, plugin: Box<dyn YummyAuthInterface>) {
         self.auth_interfaces.push(PluginInfo {
             plugin,
@@ -32,7 +60,7 @@ impl PluginExecuter {
         let mut model = model;
         for plugin in self.auth_interfaces.iter() {
             if plugin.active.load(Ordering::Relaxed) {
-                model = plugin.plugin.pre_email_auth(model)?;
+                model = plugin.plugin.pre_email_auth(self.user_manager.deref(), model)?;
             }
         }
 
@@ -43,7 +71,7 @@ impl PluginExecuter {
         let mut model = model;
         for plugin in self.auth_interfaces.iter() {
             if plugin.active.load(Ordering::Relaxed) {
-                model = plugin.plugin.post_email_auth(model)?;
+                model = plugin.plugin.post_email_auth(self.user_manager.deref(), model)?;
             }
         }
 
