b373eae388d1756d24968a046311560acdc6e3eb
Erhan BARIS <erhanbaris@gmail.com>
2023-01-26 13:01:01 +0100

    Lua code refactoring

--- interface/src/lua/mod.rs
+++ interface/src/lua/mod.rs
@@ -1,19 +1,22 @@
-use std::{rc::Rc, cell::RefCell, ops::Deref};
+mod model;
 
-use general::password::Password;
+use std::{rc::Rc, cell::RefCell, collections::HashSet};
+
 use crate::{auth::{YummyAuthInterface, YummyEmailAuthModel}, UserProxy};
 
 use mlua::prelude::*;
 
+use self::model::CallbackType;
+
 pub struct LuaYummyAuthPlugin {
     lua: Lua,
-    pre_email_auth_func: bool,
-    post_email_auth_func: bool
+    callbacks: HashSet<CallbackType>
 }
 
 impl LuaYummyAuthPlugin {
     pub fn new() -> Self {
         let lua = Lua::new();
+        let mut callbacks: HashSet<CallbackType> = HashSet::default();
         {
             let pre_email_auth_func: LuaFunction = lua.load(r#"
                 function(message)
@@ -25,6 +28,8 @@ impl LuaYummyAuthPlugin {
                     print(message:get_user_id())
                 end
             "#,).eval().unwrap();
+
+            callbacks.insert(CallbackType::PreEmailAuth);
 
             let globals = lua.globals();
             globals.set("pre_email", pre_email_auth_func).unwrap();
@@ -38,72 +43,36 @@ impl LuaYummyAuthPlugin {
                 end
             "#,).eval().unwrap();
 
+            callbacks.insert(CallbackType::PostEmailAuth);
+
             let globals = lua.globals();
             globals.set("post_email", post_email_auth_func).unwrap();
         }
 
         Self {
             lua,
-            pre_email_auth_func: true,
-            post_email_auth_func: true
+            callbacks: HashSet::default()
         }
+    }
+
+    fn execute<T: LuaUserData + 'static>(&self, model: Rc<RefCell<T>>, callback_type: CallbackType) -> anyhow::Result<()> {
+        if !self.callbacks.contains(&callback_type) {
+            return Ok(())
+        }
+
+        let func: LuaFunction = self.lua.globals().get(callback_type as u8)?;
+        func.call::<_, ()>(model)?;
+        self.lua.gc_collect()?;
+        Ok(())
     }
 }
 
 impl YummyAuthInterface for LuaYummyAuthPlugin {
     fn pre_email_auth<'a>(&self, _user_manager: &'a dyn UserProxy, model: Rc<RefCell<YummyEmailAuthModel>>) -> anyhow::Result<()> {
-        if self.pre_email_auth_func {
-            let func: LuaFunction = self.lua.globals().get("pre_email").unwrap();
-            func.call::<_, ()>(model).unwrap();
-            self.lua.gc_collect().unwrap();
-        }
-
-        Ok(())
+        self.execute(model, CallbackType::PreEmailAuth)
     }
 
     fn post_email_auth<'a>(&self, _user_manager: &'a dyn UserProxy, model: Rc<RefCell<YummyEmailAuthModel>>) -> anyhow::Result<()> {
-        if self.post_email_auth_func {
-            let func: LuaFunction = self.lua.globals().get("post_email").unwrap();
-            func.call::<_, ()>(model).unwrap();
-            self.lua.gc_collect().unwrap();
-        }
-
-        Ok(())
-    }
-}
-
-impl LuaUserData for YummyEmailAuthModel {
-    fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method("get_ref_id", |_, this, ()| Ok(this.ref_id));
-        methods.add_method("get_user_id", |_, this, ()| {
-            let user_id = match this.auth.deref() {
-                Some(auth) => auth.user.to_string(),
-                None => String::new()
-            };
-            Ok(user_id)
-        });
-        methods.add_method("get_session_id", |_, this, ()| {
-            let session_id = match this.auth.deref() {
-                Some(auth) => auth.session.to_string(),
-                None => String::new()
-            };
-            Ok(session_id)
-        });
-        methods.add_method("get_email", |_, this, ()| Ok(this.email.clone()));
-        methods.add_method("get_password", |_, this, ()| Ok(this.password.get().clone()));
-        methods.add_method("get_create", |_, this, ()| Ok(this.if_not_exist_create));
-
-        methods.add_method_mut("set_email", |_, this, email: String| {
-            this.email = email;
-            Ok(())
-        });
-        methods.add_method_mut("set_password", |_, this, password: String| {
-            this.password = Password::from(password);
-            Ok(())
-        });
-        methods.add_method_mut("set_create", |_, this, create: bool| {
-            this.if_not_exist_create = create;
-            Ok(())
-        });
+        self.execute(model, CallbackType::PostEmailAuth)
     }
 }
---
+++ interface/src/lua/model.rs
@@ -0,0 +1,49 @@
+use std::ops::Deref;
+
+use general::password::Password;
+use crate::auth::YummyEmailAuthModel;
+
+use mlua::prelude::*;
+
+#[derive(Eq, Hash, PartialEq)]
+#[repr(u8)]
+pub enum CallbackType {
+    PreEmailAuth = 1,
+    PostEmailAuth = 2
+}
+
+impl LuaUserData for YummyEmailAuthModel {
+    fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("get_ref_id", |_, this, ()| Ok(this.ref_id));
+        methods.add_method("get_user_id", |_, this, ()| {
+            let user_id = match this.auth.deref() {
+                Some(auth) => auth.user.to_string(),
+                None => String::new()
+            };
+            Ok(user_id)
+        });
+        methods.add_method("get_session_id", |_, this, ()| {
+            let session_id = match this.auth.deref() {
+                Some(auth) => auth.session.to_string(),
+                None => String::new()
+            };
+            Ok(session_id)
+        });
+        methods.add_method("get_email", |_, this, ()| Ok(this.email.clone()));
+        methods.add_method("get_password", |_, this, ()| Ok(this.password.get().clone()));
+        methods.add_method("get_create", |_, this, ()| Ok(this.if_not_exist_create));
+
+        methods.add_method_mut("set_email", |_, this, email: String| {
+            this.email = email;
+            Ok(())
+        });
+        methods.add_method_mut("set_password", |_, this, password: String| {
+            this.password = Password::from(password);
+            Ok(())
+        });
+        methods.add_method_mut("set_create", |_, this, create: bool| {
+            this.if_not_exist_create = create;
+            Ok(())
+        });
+    }
+}
