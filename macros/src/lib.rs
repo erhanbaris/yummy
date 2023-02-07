3f2a597dea21ca0857af9bc4f32d1545746b5edd
Erhan BARIS <erhanbaris@gmail.com>
2023-02-07 19:48:21 +0100

    api proc_macro removed

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -19,57 +19,6 @@ use darling::FromMeta;
 }
 
 #[proc_macro_attribute]
-pub fn api(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = parse_macro_input!(args as AttributeArgs);
-
-    let args = match MacroArgs::from_list(&args) {
-        Ok(v) => v,
-        Err(e) => { return TokenStream::from(e.write_errors()); }
-    };
-
-    let mut item: syn::Item = syn::parse(input).unwrap();
-    let fn_item = match &mut item {
-        syn::Item::Fn(fn_item) => fn_item,
-        _ => panic!("expected function")
-    };
-
-    let ItemFn { block, ..} = fn_item;
-
-    let (prepare_socket, send_message) = match args.no_socket {
-        false => {
-            (quote! { let __socket__ = model.socket.clone(); },
-             quote! {
-                if let Err(result) = response.as_ref() {
-                    __socket__.send(general::model::WebsocketMessage::fail(result.to_string()).0)
-                }
-            })
-        },
-        true => (quote! { }, quote! { }),
-    };
-
-    let block = quote! {
-        {
-            #prepare_socket
-            let mut call = || -> Self::Result {
-                #block
-            };
-    
-            let response = call();
-
-            #send_message
-            response
-        }
-    };
-
-    fn_item.block.stmts.clear();
-    fn_item.block.stmts.insert(0,syn::parse(block.into()).unwrap());
-
-    use quote::ToTokens;
-    item.into_token_stream().into()
-}
-
-
-#[proc_macro_attribute]
 pub fn plugin_api(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as AttributeArgs);
 
